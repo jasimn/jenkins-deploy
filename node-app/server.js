cd9dd1ea7e104e2a0844ee132528e65410946e89
jasimn <113772434+jasimn@users.noreply.github.com>
2025-12-19 22:39:59 +0000

    Update app response

--- node-app/server.js
+++ node-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Version3:noode.js App Deployed via Jenkins ');
+  res.send('Version2:noode.js App Deployed via Jenkins ');
 });
 
 app.listen(3000, () => {
